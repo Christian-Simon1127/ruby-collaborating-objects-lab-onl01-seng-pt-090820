fe12a8338fc742ca7c542136bc8604a204801c14
Christian Simon <csimon1127@gmail.com>
2020-10-07 17:21:48 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,34 @@
+class Song 
+  attr_accessor :name, :artist
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def artist=(artist) 
+    @artist = artist 
+    self.artist.songs << self
+  end 
+  
+  def new_by_filename(file_name)
+    files = file_name.split(" - ")
+    name = files[1]
+    name = Song.new(name)
+  end
+  
+  def artist_name
+    if @artist == nil 
+      nil 
+    else
+      self.artist.name
+    end
+  end 
+  
+end
