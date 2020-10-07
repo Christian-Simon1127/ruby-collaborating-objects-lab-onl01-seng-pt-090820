83257c287775d8b10ecdb522aa853ae72aacf9ed
Christian Simon <csimon1127@gmail.com>
2020-10-07 18:26:21 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,5 +22,19 @@ class Artist
     @songs
   end 
   
+  def self.find_or_create_by_name(name)
+    temp_artist = nil
+    Artist.all.each {|artist|
+      if artist.name = name
+        temp_artist = artist 
+      end 
+    }
+    if temp_artist == nil 
+      temp_artist = Artist.new(name)
+    end 
+    temp_artist
+  end
+  
+  
   
 end
